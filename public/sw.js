c7db39fcb5d975129f535779f757288e2ef41417
karni78 <karni78@web.de>
2026-09-06 17:43:03 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="flex1-v1";
+const CACHE="flex1-v2";
 const CORE=["/","/login","/style.css","/app.js","/manifest.webmanifest","/icons/icon-192.svg","/icons/icon-512.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
